530720827c878a541f6c402c07b3b5c1e2592b63
HryshchenkoQA <123107469+HryshchenkoQA@users.noreply.github.com>
2023-06-25 23:35:25 +0300

    Hw1

---
+++ Homework1/FirstTask.py
@@ -0,0 +1,26 @@
+'''
+Напишіть програму, в яку спочатку запишіть в 1 змінну ваше ім'я, в 2 ваше прізвище,
+а потім виводить на екран повідомлення з вашими особистими даними.
+Тут використайте конкатенацію, тобто об'єднайте стрічки.
+'''
+first_name = '   oleksandr   '
+last_name = '   hryshchenko    '
+full_name = first_name + ' ' + last_name
+capitalized_name = full_name.title()
+welcome_message = 'welcome back, dear' + ' ' + capitalized_name + ' ' + 'glad to see you again:)'
+print(welcome_message)
+
+'''
+Виведіть результат в нижньому, верхнього регістрах з капіталізацією перших букв кожного слова.
+'''
+print(welcome_message.upper())
+print(welcome_message.lower())
+print(welcome_message.title())
+
+'''
+Змініть значення своєї змінної, яку ви створили на кроці 1 , додавши до свого імені декілька пробілів на початку та вкінці.
+Прослідкуйте щоб \t \n зустрічались хоча б один раз. Виведіть нове значення. Видаліть пропуски і збережіть новий результат.
+'''
+modified_full_name = first_name.lstrip().rstrip().title() + ' ' + last_name.lstrip().rstrip().title()
+welcome_message = '\tWelcome back, dear' + ' ' + modified_full_name + ' ' + '\nGlad to see you again:)'
+print(welcome_message)
