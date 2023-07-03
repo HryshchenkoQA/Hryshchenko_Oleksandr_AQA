974165b433238ba77c50f1305b4bee15ddc1eec8
HryshchenkoQA <123107469+HryshchenkoQA@users.noreply.github.com>
2023-07-03 15:54:37 +0300

    Lesson4

---
+++ Lesson4/reverse_without_reverse().py
@@ -0,0 +1,8 @@
+list_of_numbers = [5,8,3,4,8,1,3,7,4,3]
+list_of_numbers_reverse = []
+for i in range(len(list_of_numbers)):
+    list_of_numbers_reverse.append(list_of_numbers[len(list_of_numbers) - (i + 1)])
+
+ print(list_of_numbers_reverse)
+
+    #????
