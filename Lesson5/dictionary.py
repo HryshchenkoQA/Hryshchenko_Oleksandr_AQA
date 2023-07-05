7aa4cb04e4137c3b5148d7c0273219c867f5a9fa
HryshchenkoQA <123107469+HryshchenkoQA@users.noreply.github.com>
2023-07-05 21:38:11 +0300

    Lesson5, dictionary+functions

---
+++ Lesson5/dictionary.py
@@ -0,0 +1,61 @@
+'''
+field = 'field'
+cat = {'name': 'Tom', 'age': 5, 'GoodBoy': True, 'locations': ['Home', ['yard', 'field']]}
+print(cat['name'])
+print(cat)
+print(len(cat))
+del(cat['locations'])
+print(cat)
+print('name' not in cat)
+cat2 = cat.copy()
+print(id(cat2))
+print(id(cat))
+print(cat.get('age'))
+print(cat.items())
+print(cat.keys())
+print(cat.values())
+a = cat.pop('GoodBoy')
+print(a)
+print(cat)
+print(cat.popitem())
+print(cat)
+cat.update({'fur': 'black', 'eye_color': 'blue'})
+print(cat)
+cat['tail'] = 'red'
+print(cat)
+cat.update(cat2)
+print(cat)
+
+
+first_value = 5
+second_value = first_value
+second_value = 7
+print(id(first_value))
+print(id(second_value))
+print(first_value)
+print(second_value)
+print(id(first_value))
+print(id(second_value))
+'''
+
+first_list =[1,2,3]
+second_list = first_list
+print(id(first_list))
+print(id(second_list))
+second_list[0] = 12
+print(first_list)
+print(second_list)
+print(id(first_list))
+print(id(second_list))
+print(first_list == second_list)
+print(first_list is second_list)
+#third_list = [12,2,3] or
+#third_list = first_list.copy() or
+third_list = list(first_list)
+print(id(third_list))
+print(first_list == third_list)
+print(first_list is third_list)
+third_list.append(6)
+print(first_list)
+print(third_list)
+# == проверяет значения внутри, is - является ли это одинаковые объекты
---
+++ Lesson5/introduction_to_the_functions.py
@@ -0,0 +1,38 @@
+def function_example(parametr1, parametr2 = 'im default parametr'):
+    print(f'Hello! {parametr1}')
+    print(f'and meet the {parametr2}')
+
+function_example('first', parametr2= 'a')
+function_example('second', 'b')
+function_example('third')
+
+def summ_of_two_numbers(first,second):
+    result = first + second
+    return result
+
+a = summ_of_two_numbers(5,10)
+b = summ_of_two_numbers(10,20)
+c = summ_of_two_numbers(a,b)
+print(c)
+
+def summ_and_miltiply(first,second):     #arg
+    result_of_summ = summ_of_two_numbers(first,second)
+    result_of_miltiply = first * second
+    return result_of_summ, result_of_miltiply
+first_result,second_result = summ_and_miltiply(5,20)
+print(first_result)
+print(second_result)
+
+def multiply_parametrs(*parametrs):
+    for element in parametrs:
+        print(element)
+
+multiply_parametrs('papperoni')
+multiply_parametrs('papperoni', 'carbonara', '4cheese')
+
+def multiply_named_parametrs(**user_info):    # **kwargs
+    for key, value in user_info.items():
+        print(f'{key}: {value}')
+
+multiply_named_parametrs(first_name = 'alejandro', second_name = 'formoza')
+
