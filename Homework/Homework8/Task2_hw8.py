ffb021608efc8e1c16346e74019170ee379e0492
HryshchenkoQA <123107469+HryshchenkoQA@users.noreply.github.com>
2023-07-23 12:40:56 +0300

    Lesson 10 + Homework second_task

--- Homework/Homework8/Task2_hw8.py
+++ Homework/Homework8/Task2_hw8.py
@@ -1,3 +1,14 @@
 '''
 Створіть обробку одного будь-якого exception, який не використався як приклад на занятті. Виведіть результат в консоль
 '''
+name1 = input('Enter Here YOUR !NAME!: ')
+try:
+    name1 = int(name1)
+except ValueError:
+    print('its a exception prank (: ')
+else:
+    print("Hi, {}! but it's not your name, try again :D" .format(name1))
+
+
+
+
