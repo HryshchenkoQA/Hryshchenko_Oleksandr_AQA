86edb6c41494129df69ccec632372bcb9b5e9b72
Oleksandr <Hryshchenko.qa@gmail.com>
2023-08-23 21:34:04 +0300

    HW14 second try

--- Homework/Homework14/task1.py
+++ Homework/Homework14/task1.py
@@ -2,6 +2,7 @@
 Напишите адаптер, который конвертирует json в csv. т.е. делает обратную конвертацию от той, что
 мы реализовали на уроке. Пример из урока, а также json и csv добавлено, формат записи данных тот же.
 '''
+"""
 import json
 import csv
 
@@ -32,6 +33,52 @@ converter1 = CsvConverter()
 converter2 = JsonConverter()
 converter1.read_file("example.csv")
 converter2.read_file("example.json")
+"""
+
+import csv
+import json
+
+class CsvConverter:
+    def __init__(self):
+        self.__lines = []
+
+    def read_file(self, filename: str):
+        with open(filename, 'r') as csv_file:
+            lines = csv.DictReader(csv_file)
+            for line in lines:
+                self.__lines.append(line)
+        print(self.__lines)
+
+    def write_file(self, filename: str):
+        with open(filename, 'w') as csv_file:
+            writer = csv.DictWriter(csv_file, fieldnames=self.__lines[0].keys())
+            writer.writeheader()
+            for line in self.__lines:
+                writer.writerow(line)
+
+
+class JsonConverter:
+    def __init__(self):
+        self.__lines = []
+
+    def read_file(self, filename: str):
+        with open(filename, 'r') as json_file:
+            lines = json.load(json_file)
+            for line in lines:
+                self.__lines.append(line)
+        print(self.__lines)
+
+    def write_file(self, filename: str):
+        with open(filename, 'w') as json_file:
+            json.dump(self.__lines, json_file)
+
+
+converter1 = CsvConverter()
+converter2 = JsonConverter()
+converter1.read_file("example.csv")
+converter1.write_file("example_out.csv")
+converter2.read_file("example.json")
+converter2.write_file("example_out.json")
 
 
 
